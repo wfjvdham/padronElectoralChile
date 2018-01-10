570a9a8e5ef0d744ade9351b3d0e11a6b757537e
wim <wfjvdham@gmail.com>
2018-01-10 12:36:01 -0500

    filter for files already processed

--- read_data.R
+++ read_data.R
@@ -1,27 +1,42 @@
 library("tabulizer")
 library("tidyverse")
+library("lubridate")
 
-files <- list.files(path = "../pdfs")
+pathToPdfs <- "../pdfs/"
+pathToParsedData <- "../parsed_data/"
 
-totalList <- 1:length(files) %>%
+files <- list.files(path = pathToPdfs) %>%
+  str_sub(start = 1L, end = -5L) %>%
+  as_data_frame()
+
+filesFinished <- list.files(path = pathToParsedData) %>%
+  str_sub(start = 1L, end = -5L) %>%
+  as_data_frame()
+
+filesToDo <- anti_join(files, filesFinished)
+
+totalList <- 1:nrow(filesToDo) %>%
   purrr::map(function(i_file) {
-    print(paste("file number", i_file, "/", length(files)))
+    startTime <- now()
+    print(now())
+    print(paste("file number", i_file, "/", nrow(filesToDo)))
     
-    file <- files[[i_file]]  
-    parsedFile <- extract_tables(paste0("../pdfs/", file))
-    
+    file <- filesToDo[i_file, ]  
+    parsedFile <- extract_tables(paste0(pathToPdfs, file, ".pdf"))
+
     pages <- 1:length(parsedFile) %>%
       purrr::map(function(i_page) {
-        print(paste("page number", i_page, "/", length(parsedFile)))
-        
         page <- parsedFile[[i_page]]
         colnames(page) <- page[2,]
-        page <- page[-c(1, 2), ] 
+        page <- page[-c(1, 2), ]
         page[,c("NOMBRE", "C.IDENTIDAD", "SEXO", "DOMICILIO ELECTORAL", "CIRCUNSCRIPCIÓN", "MESA")]
       })
     
-    do.call("rbind", pages)
+    total <- do.call("rbind", pages)
+    saveRDS(total, file = paste0(pathToParsedData, file, ".Rda"))
+    print(now())
   })
 
-totalDf <- do.call("rbind", totalList)
-saveRDS(total, file = "data.Rda")
+#test <- readRDS("../parsed_data/ChileChico.Rda")
+#totalDf <- do.call("rbind", totalList)
+
